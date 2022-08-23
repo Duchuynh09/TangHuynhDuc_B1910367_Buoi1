4ae6614f91458dda1b43d7e8f7d2ad7f2e7138e3
Huynh Duc <tanghuynh09@gmail.com>
2022-08-23 15:12:13 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,34 @@
+
+const app=require('./app');
+const config = require("./app/config");
+
+const PORT = config.app.port;
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+const fs = require('fs');
+const { count } = require('console');
+const { stdin } = require('process');
+
+fs.readFile('data.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  
+  console.log(data.length);
+  var count=0;
+  var start=0;
+  var space=0;
+  for(let i=0;i<data.length;i++){
+    if(data.charAt(i))
+      start=i+18;
+      console.log(data.charAt(i));
+  
+    // count++;
+    // console.log(i)
+  }
+  // console.log(count);
+  
+});
