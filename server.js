6cc7c171d103536c9ffe8790b285e5f188547063
Huynh Duc <tanghuynh09@gmail.com>
2022-08-27 20:11:31 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 
 const app=require('./app');
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util")
 
 const PORT = config.app.port;
-app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`);
-});
+
 
 const fs = require('fs');
 const { count } = require('console');
@@ -16,6 +15,21 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
     console.error(err);
     return;
   }
+async function startServer(){
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log("Connect Success")
+    const PORT = config.app.port;
+    app.listen(PORT,() => {
+      console.log(`Server running on port ${PORT}`);
+  });
+    
+  } catch (error) {
+    console.log("Connect Insuccess",error);
+    process.exit();
+  }
+}
+startServer();
   
   // console.log(data.length);
   // var count=0;
